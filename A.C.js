bde923031696dbb07efb93e69b2e6a61cec2c3e3
soumyasrishti01 <78192188+soumyasrishti01@users.noreply.github.com>
2021-11-10 16:33:36 +0530

    Add files via upload

---
+++ A.C.js
@@ -0,0 +1,28 @@
+status = "";
+img = "";
+
+function preload() {
+    img = "AC pic";
+}
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+
+    objectDetection = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("ac_detect").innerHTML = "Detecting Object";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+    }
+}
